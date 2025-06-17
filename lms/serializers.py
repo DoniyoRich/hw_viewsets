3640685c4aee04b9177bffb7becd703d6b128076
Doniyor <donish1979@gmail.com>
2025-06-17 22:45:14 +0300

    added extra field to course serializer, now calculates amount of lessons on each course

--- lms/serializers.py
+++ lms/serializers.py
@@ -1,3 +1,4 @@
+from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 
 from lms.models import Course, Lesson
@@ -18,6 +19,15 @@ class CourseSerializer(ModelSerializer):
     Сериализатор для модели курса.
     """
 
+    lessons_count = SerializerMethodField()
+
     class Meta:
         model = Course
         fields = "__all__"
+
+    @staticmethod
+    def get_lessons_count(course):
+        """
+        Дополнительное поле, вычисляет количество уроков на курсе.
+        """
+        return course.lessons.count()
